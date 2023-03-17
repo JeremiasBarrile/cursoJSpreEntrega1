71e6174a7ab40c9efd869c2857b6b03d7438d6ea
Jere <jere@MBP-de-Jere.fibertel.com.ar>
2023-03-17 17:47:20 -0300

    agrego por ultimo un lowercase y trim para facilidad del usuario

--- js/main.js
+++ js/main.js
@@ -120,19 +120,22 @@ const abonos = {
   "abono vieja amelia": 40000,
   "abono platea este": 60000,
   "abono palomar": 20000,
-  };
-  
-  const DESC = 0.30;
-  
+};
+
+let DESC = 0.30;
+
+function calculoAbonos() {
   let abonoElegido = "";
-  while (abonoElegido !== "salir") {
-  abonoElegido = prompt("¿Qué abono desea?");
-  if (abonos[abonoElegido]) {
-  console.log("Su abono a comprar es:", abonoElegido);
-  const precioConDesc = abonos[abonoElegido] * (1 - DESC);
-  const precioFinal = precioConDesc.toFixed(2);
-  console.log("El precio de su abono es de:", precioFinal);
-  } else if (abonoElegido !== "salir") {
-  alert("No tenemos ningún abono con ese nombre, intente nuevamente");
+  while (abonoElegido.toLowerCase().trim() !== "salir") {
+    abonoElegido = prompt("¿Qué abono desea?");
+    abonoElegido = abonoElegido.toLowerCase().trim(); 
+    if (abonos[abonoElegido]) {
+      console.log("Su abono a comprar es:", abonoElegido);
+      const precioConDesc = abonos[abonoElegido] * (1 - DESC);
+      const precioFinal = precioConDesc.toFixed(2);
+      console.log("El precio de su abono es de:", precioFinal);
+    } else if (abonoElegido !== "salir") {
+      alert("No tenemos ningún abono con ese nombre, intente nuevamente");
+    }
   }
-  }
+}
